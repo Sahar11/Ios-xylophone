40faa55a33af92366cf8ddfa4dc9f74ce795edbc
Sahar11 <saharhalim151@gmail.com>
2022-11-11 09:31:08 -0500

    add music in viewController

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -14,12 +14,13 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         
-        print(sender.currentTitle)
-        playSound()
+        //print(sender.currentTitle)
+        sender.alpha = 0.5
+        playSound(soundName: sender.currentTitle!)
     }
 
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
 
